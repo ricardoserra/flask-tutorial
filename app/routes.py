c06f5446658b114aa8d09c0378327794528117bd
rserra <ricardo@mediagaps.com>
2018-10-08 18:01:59 +0100

    Added templates from second part

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,16 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello World!"
+    user = {'username': 'Miguel'}
+    posts =[
+        {'author' : {'username': "Ricardo"},
+        'body' : "Some lipsum ipsum text"},
+        {
+            'author' : {'username': 'Serra'},
+            'body' : "Some text"
+        }
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts)
