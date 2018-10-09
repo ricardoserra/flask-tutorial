a1e50af035ec446888e017c8f95af67c40066929
rserra <ricardo@mediagaps.com>
2018-10-09 17:55:38 +0100

    added initial SQLAlchemy config and two tables: User and Post

--- config.py
+++ config.py
@@ -1,4 +1,8 @@
 import os
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config:
     SECRET_KEY = 'my unique k3y'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+        'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
